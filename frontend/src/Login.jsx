4d7a9dc2fe4f86ed43d55afdf1bc7e6a9563a4f9
Kavish Nayeem <nayeemda0411@gmail.com>
2025-05-05 15:52:43 -0500

    Done with Downward Contrast

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -53,7 +53,7 @@ const Login = () => {
           <h4>Enter 2 for Upward Contrast (done)</h4>
           <h4>Enter 3 for Neutral (done)</h4>
           <h4>Enter 4 for Downward Assimilation(done)</h4>
-          <h4>Enter 5 for Downward Contrast (in progress)</h4>
+          <h4>Enter 5 for Downward Contrast (done)</h4>
           <form onSubmit={handleLogin} className="passkey-form">
             <input
               type="password"
